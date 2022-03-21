9bcc7037ecdf477adf66a3a50064a88a65330dda
Administrator <aya.tawfik@gmail.com>
2022-03-21 12:05:07 +0200

    test

--- src/test/java/API_Test.java
+++ src/test/java/API_Test.java
@@ -13,7 +13,7 @@ import static io.restassured.RestAssured.given;
 
 public class API_Test
 {
-	//@Test
+	@Test
 	public void Testcase1() {
 		RequestSpecification request = RestAssured.given();
 		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
